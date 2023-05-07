88563b639b4fc476369300af301e0455d51bf772
Farhad <ferhad096@gmail.com>
2023-05-07 14:59:31 +0400

    lst

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1061,14 +1061,7 @@
                         <li><a href="#rs-contact">Contact</a></li>
                     </ul>
                 </div>
-                <!-- <div class="col-lg-3">
-                    <h3 class="footer-title">Newsletter</h3>
-                    <p          class="widget-desc">Tetur adipiscing elit, sed do eiusmod tempor in cididunt ut labore.</p>
-{{--                    <p>--}}
-{{--                        <input type="email" name="email" placeholder="Your Email Address" required="">--}}
-{{--                        <input type="submit" value="Subscribe Now">--}}
-{{--                    </p>--}}
-                </div> -->
+
             </div>
         </div>
     </div>
